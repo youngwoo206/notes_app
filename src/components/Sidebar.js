68e0bf70b7cb2e59c509e733c63296e68477b499
Youngwoo <youngwoo206@gmail.com>
2022-10-25 20:21:34 -0400

    modal doesnt work ;(

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -26,13 +26,61 @@ export default function Sidebar(props) {
         <h3>Notes</h3>
       </div>
       <div className="toolbar">
-        <button className="new-note" onClick={props.newNote}>
-          +
+        <button className="toolbar-btn" onClick={props.newNote}>
+          New Note
+        </button>
+        <button className="toolbar-btn">New Folder</button>
+        <button className="toolbar-btn">Shortcuts</button>
+        <button className="toolbar-btn">Log Out</button>
+
+        <button
+          type="button"
+          class="btn btn-primary"
+          data-toggle="modal"
+          data-target="#exampleModal"
+        >
+          Launch demo modal
         </button>
 
-        <button>new folder</button>
-        <button>shortcuts</button>
-        <button>log out</button>
+        <div
+          class="modal fade"
+          id="exampleModal"
+          tabindex="-1"
+          role="dialog"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">
+                  Modal title
+                </h5>
+                <button
+                  type="button"
+                  class="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body"></div>
+              <div class="modal-footer">
+                <button
+                  type="button"
+                  class="btn btn-secondary"
+                  data-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button type="button" class="btn btn-primary">
+                  Save changes
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       {noteElements}
     </section>
